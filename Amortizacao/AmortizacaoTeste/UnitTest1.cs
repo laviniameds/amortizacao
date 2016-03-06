2b88977e8f3243bd16865c443386a7387897938c
laviniameds <laviniameds@icloud.com>
2016-03-06 15:09:26 -0300

    1º

---
+++ Amortizacao/AmortizacaoTeste/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Amortizacao;
+
+namespace AmortizacaoTeste
+{
+    [TestClass]
+    public class AmortizacaoTeste
+    {
+        [TestMethod]
+        public void Construtor()
+        {
+            
+        }
+    }
+}
